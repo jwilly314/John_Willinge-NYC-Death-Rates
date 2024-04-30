c2f84887c3ccedb48dae1a5a1cde5be6c2dd4b1b
jwilly314 <153216247+jwilly314@users.noreply.github.com>
2024-04-30 13:38:24 -0400

    Add files via upload

---
+++ dataJournalism/server.py
@@ -0,0 +1,109 @@
+# flask --app data_server run
+from flask import Flask
+from flask import render_template
+from flask import request
+import json
+
+
+app = Flask(__name__, static_url_path='', static_folder='static')
+
+@app.route('/')
+def about():
+    return render_template('about.html')
+
+@app.route('/about')
+def about2():
+    return render_template('about.html')
+
+@app.route('/macro')
+def macro():
+    f = open("_data/nycMortality_area.json", "r")
+    data = json.load(f)
+    f.close()
+
+    #Filter and reformat data for ease of access in the template
+    neighborhoods = list(data.keys())
+    print(neighborhoods)
+    requested_data = {}
+    for neighborhood in neighborhoods:
+        requested_data[neighborhood] = data[neighborhood]
+
+    fills = []
+    for neighborhood in neighborhoods:
+        if 0.5 <= float(data[neighborhood][0]) < 0.75:
+            fills.append("#ff0000")
+        elif 0.75 <= float(data[neighborhood][0]) < 1:
+            fills.append("#e60000")
+        elif 1 <= float(data[neighborhood][0]) < 1.25:
+            fills.append("#cc0000")
+        elif 1.25 <= float(data[neighborhood][0]) < 1.5:
+            fills.append("#b30000")
+        elif 1.5 <= float(data[neighborhood][0]) < 1.75:
+            fills.append("#990000")
+        elif 1.75 <= float(data[neighborhood][0]) < 2:
+            fills.append("#800000")
+        elif 2 <= float(data[neighborhood][0]) < 2.25:
+            fills.append("#660000")
+        elif 2.25 <= float(data[neighborhood][0]) < 2.5:
+            fills.append("#4d0000")
+        elif 2.5 <= float(data[neighborhood][0]) < 2.75:
+            fills.append("#330000")
+        elif 2.75 <= float(data[neighborhood][0]) < 3:
+            fills.append("#190000")
+    print(fills)
+
+    return render_template('macro.html', data=requested_data, fills=fills)
+
+
+@app.route('/manhattan')
+def micro1():
+    f = open("_data/nycMortality_area.json", "r")
+    data = json.load(f)
+    f.close()
+
+    g = open("_data/nycMortality_borough.json", "r")
+    data2 = json.load(g)
+    g.close()
+
+    #Filter and reformat data for ease of access in the template
+    boroughs = list(data2.keys())
+
+    averageData = 0
+
+    for borough in boroughs:
+        for value in data2[borough]:
+            averageData += float(value)
+
+    averageData = averageData/50
+
+    manhattanPoints = ""
+
+    year = 2007
+    for value in data2["Manhattan"]:
+        manhattanPoints = manhattanPoints + str(40+((year-2007)*23.3333)) + "," + str(260-(float(value))*20) + " "
+        year += 1
+        if year==2016:
+            finalPoint = str(260-(float(value))*20)
+
+        
+    manhattanPoints = manhattanPoints[:-1]
+
+    return render_template('microManhattan.html', data=data, data2=data2, averageData = 260-(averageData*20), numericalAverage=averageData, manhattanPoints=manhattanPoints, finalPoint=float(finalPoint)+5.5)
+
+@app.route('/bronx')
+def micro2():
+    return render_template('microBronx.html')
+
+@app.route('/queens')
+def micro3():
+    return render_template('microQueens.html')
+
+@app.route('/brooklyn')
+def micro4():
+    return render_template('microBrooklyn.html')
+
+@app.route('/staten')
+def micro5():
+    return render_template('microStaten.html')
+
+app.run(debug=True, port=6060)
